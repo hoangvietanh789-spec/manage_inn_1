321b2d51c39fd7ecfa1005d7bbc238f6cc6f923e
hoangvietanh789-spec <hoangvietanh789@gmail.com>
2025-08-26 19:16:59 +0700

    Update core.py

--- manage/core.py
+++ manage/core.py
@@ -133,7 +133,11 @@ def run(*month_input):
     
     # Xuất dữ liệu ra Excel bằng pandas + openpyxl
     df = pd.DataFrame(all_records)
-    df['notify'] = df.apply(lambda row: row['notify'] if row['due_amount'] not in [0,'0'] else '', axis = 1) # bỏ nội dung thông báo ở các dòng đã thanh toán hết
+    df['notify'] = df.apply(lambda row: row['notify'] if row['due_amount'] not in [0,'0'] else '', axis = 1) # bỏ notify các dòng đẫ thanh toán
+    df = df[df['status'] != 'available'] # bỏ bớt các dòng chưa cho thuê
+    col_remain = list(df.columns) 
+    col_remain.remove('status')
+    df = df[col_remain]
     df.to_excel(file_report, index=False, sheet_name="Report", engine="openpyxl")
     # Mở lại file bằng openpyxl để chỉnh sửa
     wb = load_workbook(file_report)
