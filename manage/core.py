6efaea4cb9bff948d2531f690a30e98636097d54
hoangvietanh789-spec <hoangvietanh789@gmail.com>
2026-09-03 22:56:40 +0700

    Update core.py

--- manage/core.py
+++ manage/core.py
@@ -108,7 +108,7 @@ def tinhtien(*month_input):
 
     data = query('rooms')
     all_records = []
-    print(list(data.keys())[-1:-3])
+    print(list(data.keys()).sort()[:3])
     month_tocal = [this_month] 
     if len(month_input) == 0:
         ask = input("Month to calculate [add / all]: ")
